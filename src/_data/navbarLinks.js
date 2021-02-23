05a4eaa4c60d3df4672212d5c9fa74758ad8217d
Deepak Gupta <deepakgupta191199@gmail.com>
2021-02-23 17:27:19 +0530

    changes

---
+++ src/_data/navbarLinks.js
@@ -0,0 +1,22 @@
+module.exports = [
+    {
+        name: 'Home',
+        url: '/'
+    },
+    {
+        name: "Resources",
+        url: "/resources/"
+    },
+    {
+        name: "Blog",
+        url: "/blog/"
+    },
+    {
+        name: "Team",
+        url: "/team/"
+    },
+    {
+        name: "Contact Us",
+        url: "/contact/"
+    }
+]
--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,1 +1,5 @@
 import "./_bootstrap.js"
+
+// TODO: Does not work for nested links
+var pathname = window.location.pathname;
+const navLinks = document.querySelector(`.navbar-nav li a[href="${pathname}"]`)?.classList?.add('active');
