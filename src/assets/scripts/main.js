4711a9963ec36ff38555d0e41c91fc72a0a2e54a
shagir <husainshagir98@gmail.com>
2021-02-25 16:06:03 +0530

    about

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,5 +1,4 @@
 import "./_bootstrap.js"
-
 // TODO: Does not work for nested links
 var pathname = window.location.pathname;
 const navLinks = document.querySelector(`.navbar-nav li a[href="${pathname}"]`)?.classList?.add('active');
@@ -10,7 +9,7 @@ document.querySelectorAll(".smoothScroll").forEach(item => {
     item.addEventListener('click', e => {
     e.preventDefault()
     const href = e.currentTarget.getAttribute('href')
-    const offsetTop = document.querySelector(href).offsetTop - 80
+    const offsetTop = document.querySelector(href).offsetTop - 20
     scroll({
       top: offsetTop,
       behavior: 'smooth'
