30d1fd6a7ed4e686e029ce9f506f46c867250144
shagir <husainshagir98@gmail.com>
2021-04-17 08:06:17 +0530

    newevent

---
+++ src/_data/events.js
@@ -0,0 +1,22 @@
+module.exports = [
+    {
+        name: "One",
+        url: "https://www.youtube.com/embed/NGAKKkt8FVE",
+        description: "event 1"
+    },
+    {
+        name: "Two",
+        url: "https://www.youtube.com/embed/aQDKfS3pkDc",
+        description: "event 2"
+    },
+    {
+        name: "Three",
+        url: "https://www.youtube.com/embed/2B4hvRgPib0",
+        description: "event 3"
+    },  {
+        name: "Four",
+        url: "https://www.youtube.com/embed/WRimOhKK1c8",
+        description: "event 4"
+    },
+
+]
