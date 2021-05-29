413f1ed71489e20f55bb3e6f81c6277ee6ca2686
prashusharma <prashukumarsharmassm2000@gmail.com>
2021-05-29 15:37:10 +0530

    teams updated

---
+++ src/_data/teams.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        name: "Prashu",
+        url: "#",
+        image: "/assets/images/landing.jpg",
+        description: "We do Workshops"
+    },
+    {
+        name: "Sumit",
+        url: "#",
+        image: "/assets/images/landing.jpg",
+        description: "We do Workshops"
+    },
+    {
+        name: "Ankit",
+        url: "#",
+        image: "/assets/images/landing.jpg",
+        description: "Innovation is born out of community interaction and the freedom to learn and explore. Students are given an opportunity to find their passion and build something of their own in a like-minded environment. <br/>Tech events comprise ardent competitions, exhibitions, illuminating talks, quizzes, interactions, and many more that inspire the students to bring out their hidden talent."
+    },
+    {
+        name: "Study Resources",
+        url: "#",
+        image: "/assets/images/landing.jpg",
+        description: "We do Workshops"
+    },
+    {
+        name: "Expert Lecture Sessions",
+        url: "#",
+        image: "/assets/images/landing.jpg",
+        description: "Various expert lecture sessions are organized so that the students can excel in technology, creativity, and lifelong learning. These sessions will include talks on problem-led research which will generate transformative ideas and will improve the critical thinking of the students attending it. Such sessions focus on innovation, problem-solving, teamwork, and other aspects of technology."
+    },
+    {
+        name: "Case Studies",
+        url: "#",
+        image: "/assets/images/landing.jpg",
+        description: "We do Workshops"
+    }
+]
