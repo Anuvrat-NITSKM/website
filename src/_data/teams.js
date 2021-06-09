5411bf0aa39ea8e70dfab2c1d8250e629c8c6f66
Deepak Gupta <deepakgupta191199@gmail.com>
2021-06-09 11:15:47 +0530

    styling changes and responsive cards

--- src/_data/teams.js
+++ src/_data/teams.js
@@ -11,7 +11,7 @@ module.exports = {
       Linkedin: "https://www.linkedin.com/in/deepak-choudhary-23728b170",
       Facebook: "https://www.facebook.com/deepakthegod07",
       Description: "",
-      photo: "/assets/images/image/Deepak Choudhary.png"
+      photo: "/assets/images/image/Deepak choudhary.png"
     },
     {
       Timestamp: "4/21/2021 13:57:45",
@@ -50,116 +50,116 @@ module.exports = {
       photo: "/assets/images/image/girija.jpg"
     }
   ],
-  // volunteers: [{
-  //     Timestamp: "4/20/2021 23:25:18",
-  //     Email: "b190068@nitsikkim.ac.in",
-  //     Name: "Srishti Prasad",
-  //     Email: "b190068@nitsikkim.ac.in",
-  //     Role: "Volunteer",
-  //     Instagram: "https://instagram.com/__srishti_26?igshid=6t7v8nie7hb2",
-  //     Linkedin: "https://www.linkedin.com/in/srishti-prasad-436581209",
-  //     Facebook: "https://www.facebook.com/srishti.prasad.90",
-  //     Description: "I regard myself to be very optimistic, determined, focused and responsible person.",
-  //     photo: "/assets/images/image/srishti.png"
-  //   },
-    
-  //   {
-  //     Timestamp: "4/20/2021 14:01:10",
-  //     Email: "B190043@nitsikkim.ac.in",
-  //     Name: "Prashu Kumar Sharma",
-  //     Email: "b190043@nitsikkim.ac.in",
-  //     Role: "Volunteer",
-  //     Instagram: "https://www.instagram.com/_prashu.sharma_/",
-  //     Linkedin: "https://www.linkedin.com/in/prashu-kumar-sharma-10679020b/",
-  //     Facebook: "https://www.facebook.com/pk.raj.756",
-  //     Description: "I’m organized. I really enjoy working with a wide variety of people to achieve a common goal efficiently and realistically.",
-  //     photo: "/assets/images/image/Prashu.png"
-  //   },
-  //   {
-  //     Timestamp: "4/21/2021 14:33:33",
-  //     Email: "b200094@nitsikkim.ac.in",
-  //     Name: "Anirudh Ghildiyal",
-  //     Email: "b200094@nitsikkim.ac.in",
-  //     Role: "Volunteer",
-  //     Instagram: "",
-  //     Linkedin: "https://www.linkedin.com/in/anirudh-ghildiyal-19338520a/",
-  //     Facebook: "",
-  //     Description: "",
-  //     photo: "/assets/images/image/Anirudh"
-  //   },
-  //   {
-  //     Timestamp: "4/20/2021 14:32:11",
-  //     Email: "b190061@nitsikkim.ac.in",
-  //     Name: "Raunak Singh",
-  //     Email: "b190061@nitsikkim.ac.in",
-  //     Role: "Volunteer",
-  //     Instagram: "https://www.instagram.com/rau_nak_/",
-  //     Linkedin: "https://linkedin.com/in/raunak-singh-91b4891b1/",
-  //     Facebook: "I don't have Facebook ID.",
-  //     Description: "An architect of life who builds its foundation and choose its contents, a simple mind with a versatile nature and just a student who wants to touch the heights of innovation and technology.",
-  //     photo: "/assets/images/image/Raunak.png"
-  //   },
-  //   {
-  //     Timestamp: "4/20/2021 17:41:10",
-  //     Email: "b20¬¬0098@nitsikkim.ac.in",
-  //     Name: "Sakshee Rajpoot",
-  //     Email: "b200098@nitsikkim.ac.in",
-  //     Role: "Volunteer",
-  //     Instagram: "",
-  //     Linkedin: "https://www.linkedin.com/in/sakshee-rajpoot-0593b0197",
-  //     Facebook: "",
-  //     Description: "",
-  //     photo: "/assets/images/image/Sakshee.png"
-  //   },
-  //   {
-  //     Timestamp: "4/21/2021 10:25:53",
-  //     Email: "b200084@nitsikkim.ac.in",
-  //     Name: "Riya Mathur",
-  //     Email: "b200084@nitsikkim.ac.in",
-  //     Role: "Volunteer",
-  //     Instagram: "https://www.instagram.com/riiyamathur/",
-  //     Linkedin: "https://www.linkedin.com/in/riya-mathur-2827ba1ba",
-  //     Facebook: "",
-  //     Description: "",
-  //     photo: "/assets/images/image/Riya.png"
-  //   },
-  //   {
-  //     Timestamp: "4/21/2021 11:55:53",
-  //     Email: "b190044@nitsikkim.ac.in",
-  //     Name: "Vanka Janaki Rama Santhosh",
-  //     Email: "b190044@nitsikkim.ac.in",
-  //     Role: "Volunteer",
-  //     Instagram: "https://www.instagram.com/santhosh_chowdary_vanka?r=nametag",
-  //     Linkedin: "https://www.linkedin.com/in/janaki-rama-santhosh-vanka-6aba4719a",
-  //     Facebook: "",
-  //     Description: "Think Practically",
-  //     photo: "/assets/images/image/VankiJanaRamaSantosh.png"
-  //   },
-  //   {
-  //     Timestamp: "4/21/2021 12:10:34",
-  //     Email: "b200089@nitsikkim.ac.in",
-  //     Name: "Adarsh Kumar",
-  //     Email: "b200089@nitsikkim.ac.in",
-  //     Role: "Volunteer",
-  //     Instagram: "",
-  //     Linkedin: "https://www.linkedin.com/in/adarsh-kumar-896a2920a",
-  //     Facebook: "",
-  //     Description: "",
-  //     photo: "/assets/images/image/Adarsh.jpg"
-  //   },
-  //   {
-  //     Timestamp: "4/20/2021 14:01:10",
-  //     Email: "b200076@nitsikkim.ac.in",
-  //     Name: "Siddharth Utsav",
-  //     Email: "b200076@nitsikkim.ac.in",
-  //     Role: "Volunteer",
-  //     Instagram: "https://instagram.com/_siddharth_uk_37_?igshid=1fs6icfeuzrv4",
-  //     Linkedin: "https://www.linkedin.com/in/siddharth-utsav-karna-53a3691a6",
-  //     Facebook: "https://www.facebook.com/profile.php?id=100005532588384",
-  //     Description: "I’m organized. I really enjoy working with a wide variety of people to achieve a common goal efficiently and realistically.",
-  //     photo: "/assets/images/image/Siddhart Utsav.png"
-  //   },
-  // ],
+  volunteers: [{
+      Timestamp: "4/20/2021 23:25:18",
+      Email: "b190068@nitsikkim.ac.in",
+      Name: "Srishti Prasad",
+      Email: "b190068@nitsikkim.ac.in",
+      Role: "Volunteer",
+      Instagram: "https://instagram.com/__srishti_26?igshid=6t7v8nie7hb2",
+      Linkedin: "https://www.linkedin.com/in/srishti-prasad-436581209",
+      Facebook: "https://www.facebook.com/srishti.prasad.90",
+      Description: "I regard myself to be very optimistic, determined, focused and responsible person.",
+      photo: "/assets/images/image/srishti.png"
+    },
+
+    {
+      Timestamp: "4/20/2021 14:01:10",
+      Email: "B190043@nitsikkim.ac.in",
+      Name: "Prashu Kumar Sharma",
+      Email: "b190043@nitsikkim.ac.in",
+      Role: "Volunteer",
+      Instagram: "https://www.instagram.com/_prashu.sharma_/",
+      Linkedin: "https://www.linkedin.com/in/prashu-kumar-sharma-10679020b/",
+      Facebook: "https://www.facebook.com/pk.raj.756",
+      Description: "I’m organized. I really enjoy working with a wide variety of people to achieve a common goal efficiently and realistically.",
+      photo: "/assets/images/image/Prashu.png"
+    },
+    {
+      Timestamp: "4/21/2021 14:33:33",
+      Email: "b200094@nitsikkim.ac.in",
+      Name: "Anirudh Ghildiyal",
+      Email: "b200094@nitsikkim.ac.in",
+      Role: "Volunteer",
+      Instagram: "",
+      Linkedin: "https://www.linkedin.com/in/anirudh-ghildiyal-19338520a/",
+      Facebook: "",
+      Description: "",
+      photo: "/assets/images/image/Anirudh"
+    },
+    {
+      Timestamp: "4/20/2021 14:32:11",
+      Email: "b190061@nitsikkim.ac.in",
+      Name: "Raunak Singh",
+      Email: "b190061@nitsikkim.ac.in",
+      Role: "Volunteer",
+      Instagram: "https://www.instagram.com/rau_nak_/",
+      Linkedin: "https://linkedin.com/in/raunak-singh-91b4891b1/",
+      Facebook: "I don't have Facebook ID.",
+      Description: "An architect of life who builds its foundation and choose its contents, a simple mind with a versatile nature and just a student who wants to touch the heights of innovation and technology.",
+      photo: "/assets/images/image/Raunak.png"
+    },
+    {
+      Timestamp: "4/20/2021 17:41:10",
+      Email: "b20¬¬0098@nitsikkim.ac.in",
+      Name: "Sakshee Rajpoot",
+      Email: "b200098@nitsikkim.ac.in",
+      Role: "Volunteer",
+      Instagram: "",
+      Linkedin: "https://www.linkedin.com/in/sakshee-rajpoot-0593b0197",
+      Facebook: "",
+      Description: "",
+      photo: "/assets/images/image/Sakshee.png"
+    },
+    {
+      Timestamp: "4/21/2021 10:25:53",
+      Email: "b200084@nitsikkim.ac.in",
+      Name: "Riya Mathur",
+      Email: "b200084@nitsikkim.ac.in",
+      Role: "Volunteer",
+      Instagram: "https://www.instagram.com/riiyamathur/",
+      Linkedin: "https://www.linkedin.com/in/riya-mathur-2827ba1ba",
+      Facebook: "",
+      Description: "",
+      photo: "/assets/images/image/Riya.png"
+    },
+    {
+      Timestamp: "4/21/2021 11:55:53",
+      Email: "b190044@nitsikkim.ac.in",
+      Name: "Vanka Janaki Rama Santhosh",
+      Email: "b190044@nitsikkim.ac.in",
+      Role: "Volunteer",
+      Instagram: "https://www.instagram.com/santhosh_chowdary_vanka?r=nametag",
+      Linkedin: "https://www.linkedin.com/in/janaki-rama-santhosh-vanka-6aba4719a",
+      Facebook: "",
+      Description: "Think Practically",
+      photo: "/assets/images/image/VankiJanaRamaSantosh.png"
+    },
+    {
+      Timestamp: "4/21/2021 12:10:34",
+      Email: "b200089@nitsikkim.ac.in",
+      Name: "Adarsh Kumar",
+      Email: "b200089@nitsikkim.ac.in",
+      Role: "Volunteer",
+      Instagram: "",
+      Linkedin: "https://www.linkedin.com/in/adarsh-kumar-896a2920a",
+      Facebook: "",
+      Description: "",
+      photo: "/assets/images/image/Adarsh.jpg"
+    },
+    {
+      Timestamp: "4/20/2021 14:01:10",
+      Email: "b200076@nitsikkim.ac.in",
+      Name: "Siddharth Utsav",
+      Email: "b200076@nitsikkim.ac.in",
+      Role: "Volunteer",
+      Instagram: "https://instagram.com/_siddharth_uk_37_?igshid=1fs6icfeuzrv4",
+      Linkedin: "https://www.linkedin.com/in/siddharth-utsav-karna-53a3691a6",
+      Facebook: "https://www.facebook.com/profile.php?id=100005532588384",
+      Description: "I’m organized. I really enjoy working with a wide variety of people to achieve a common goal efficiently and realistically.",
+      photo: "/assets/images/image/Siddhart Utsav.png"
+    },
+  ],
   coordinators: [{
       Timestamp: "4/21/2021 14:52:37",
       Email: "b180093@nitsikkim.ac.in",
@@ -204,7 +204,6 @@ module.exports = {
       Role: "Resource Management Team",
       Instagram: "https://www.instagram.com/sanjeevshrmaa/",
       Linkedin: "https://www.linkedin.com/in/sanjeevshrmaa/",
-      Facebook: "I am not on FB",
       Description: " It is not enough that I should succeed -- others should fail ",
       photo: "/assets/images/image/Sanjeev kumar.png"
     },
@@ -245,7 +244,7 @@ module.exports = {
       photo: "/assets/images/image/Aanchal.png"
     },
   ],
-  
+
   head: [{
     Timestamp: "4/20/2021 19:05:20",
     Email: "b180099@nitsikkim.ac.in",
